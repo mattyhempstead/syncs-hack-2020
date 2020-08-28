19ce2cc9e3a87f1229fc708c0ddcf5d575083140
pranavalavandi <pranav.alavandi>
2020-08-29 00:36:25 +1000

    Encoding

---
+++ test.py
@@ -0,0 +1,20 @@
+import binascii
+
+
+
+text = "https://www.google.com/"
+binary_conversion = bin(int.from_bytes(text.encode(), 'big'))
+binary_conversion = binary_conversion[2:]
+
+
+
+for count,i in enumerate(binary_conversion):
+    time = 0.5
+    if count%2 == 0:
+        base_one = 220
+        base_two = 440
+    else:
+        base_one = 320
+        base_two = 550
+
+    if i == "0":
