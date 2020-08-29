0e9ea0f799403802a9fc46cb42e2e9d9d5b00d47
Cory Aitchison <corytaitchison@gmail.com>
2020-08-29 13:58:22 +1000

    Remove comment

--- test2/src/components/FormInput/index.js
+++ test2/src/components/FormInput/index.js
@@ -1,26 +1,8 @@
 import React, { useState } from "react";
 import "./index.scss";
 
-// const FormInput = () => {
-//   return (
-// <label className="mdc-text-field mdc-text-field--outlined">
-//   <input
-//     type="text"
-//     className="mdc-text-field__input"
-//     aria-labelledby="my-label-id"
-//     onChange={console.log("hi")}
-//   />
-//   <span className="mdc-notched-outline">
-//     <span className="mdc-notched-outline__leading"></span>
-//     <span className="mdc-notched-outline__notch"></span>
-//     <span className="mdc-notched-outline__trailing"></span>
-//   </span>
-// </label>
-//   );
-// };
-
 const FormInput = () => {
-  const [msg, setMsg] = useState("type here");
+  const [msg, setMsg] = useState("type here...");
 
   return (
     <input
