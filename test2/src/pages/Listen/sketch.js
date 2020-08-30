5cdf1b3fd324a5996edf4993ed0e0a08b1ed1c2c
Cory Aitchison <corytaitchison@gmail.com>
2020-08-30 10:20:45 +1000

    Ascend the spactime continuum

--- test2/src/pages/Listen/sketch.js
+++ test2/src/pages/Listen/sketch.js
@@ -45,243 +45,235 @@ let freqs = [];
 let passMsg = () => {};
 
 const decodeBits = () => {
-    let frequencyHeard = [];
+  let frequencyHeard = [];
 
-    for (let i in dataArray) {
-        if (dataArray[i] > INTENSITY_THRESHOLD) {
-            frequencyHeard.push(freqs[i]);
-        }
+  for (let i in dataArray) {
+    if (dataArray[i] > INTENSITY_THRESHOLD) {
+      frequencyHeard.push(freqs[i]);
     }
+  }
 
-    let bitArray = [];
+  let bitArray = [];
 
-    for (let i of frequencyHeard) {
-        for (let j = 0; j < FREQUENCY_BUCKETS.length; j++) {
-            if (bitArray.includes(FREQUENCY_BUCKETS[j])) continue;
-            if (Math.abs(i - FREQUENCY_BUCKETS[j]) < FREQUENCY_TOLERANCE) {
-                bitArray.push(FREQUENCY_BUCKETS[j]);
-            }
-        }
+  for (let i of frequencyHeard) {
+    for (let j = 0; j < FREQUENCY_BUCKETS.length; j++) {
+      if (bitArray.includes(FREQUENCY_BUCKETS[j])) continue;
+      if (Math.abs(i - FREQUENCY_BUCKETS[j]) < FREQUENCY_TOLERANCE) {
+        bitArray.push(FREQUENCY_BUCKETS[j]);
+      }
     }
+  }
 
-    let finalInt = 0;
-    for (let i of bitArray) {
-        finalInt += 2 ** (7 - FREQUENCY_BUCKETS.indexOf(i));
-        //finalInt += frequencyMap[i]
-    }
+  let finalInt = 0;
+  for (let i of bitArray) {
+    finalInt += 2 ** (7 - FREQUENCY_BUCKETS.indexOf(i));
+    //finalInt += frequencyMap[i]
+  }
 
-    return finalInt;
+  return finalInt;
 };
 
 const recalculateFourier = () => {
-    analyserNode.getByteFrequencyData(dataArray);
-    dataArray.map((i) => i ** 2);
-    decode_message(decodeBits());
+  analyserNode.getByteFrequencyData(dataArray);
+  dataArray.map((i) => i ** 2);
+  decode_message(decodeBits());
 };
 
 const sketch = (p) => {
-    let screenWidth = window.innerWidth;
-    let xc = screenWidth / 2;
-    let yc = 250;
-    let r = screenWidth > 400 ? 64 : screenWidth / (2 * Math.PI);
+  let screenWidth = window.innerWidth;
+  let xc = screenWidth / 2;
+  let yc = 250;
+  let r = screenWidth > 400 ? 64 : screenWidth / (2 * Math.PI);
 
-    p.setup = () => {
-        p.createCanvas(screenWidth, screenWidth * 2.5);
-        p.frameRate(framerate);
-        p.loop();
-    };
+  p.setup = () => {
+    p.createCanvas(screenWidth, screenWidth * 2.5);
+    p.frameRate(framerate);
+    p.loop();
+  };
 
-    p.mouseClicked = () => {
-        if ((p.mouseX - xc) ** 2 + (p.mouseY - yc) ** 2 <= r ** 2) {
-            initAudio();
-        }
-    };
+  p.mouseClicked = () => {
+    if ((p.mouseX - xc) ** 2 + (p.mouseY - yc) ** 2 <= r ** 2 && !started) {
+      initAudio();
+    }
+  };
 
-    let t = 0;
+  p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
+    passMsg = props.updateFunction;
+  };
 
-    p.draw = () => {
-        p.background("rgb(154, 209, 223)");
-        if (!started) {
-            p.fill("rgb(255, 255, 255)");
-            p.circle(xc, yc, 2 * r);
-            p.textSize(32);
-            p.fill("rgb(0, 0, 0)");
-            p.textAlign(p.CENTER, p.CENTER);
-            p.textFont("Amatic SC", 50);
-            p.text("start", xc, yc);
-            return;
-        }
-        let dlen = dataArray.length;
-        t -= 0.01;
-        timer++;
+  let t = 0;
 
-        //console.log(decodeBits());
-        //console.log('data', dataArray);
-        //let index = dataArray.indexOf(Math.max(...dataArray));
-        //console.log(freqs[index]);
+  p.draw = () => {
+    p.background("rgb(154, 209, 223)");
+    if (!started) {
+      p.fill("rgb(255, 255, 255)");
+      p.circle(xc, yc, 2 * r);
+      p.textSize(32);
+      p.fill("rgb(0, 0, 0)");
+      p.textAlign(p.CENTER, p.CENTER);
+      p.textFont("Amatic SC", 50);
+      p.text("start", xc, yc);
+      return;
+    }
+    let dlen = dataArray.length;
+    t -= 0.01;
+    timer++;
 
-        for (let i in dataArray) {
-            if (dataArray[i] > INTENSITY_THRESHOLD) {
-                // console.log(i);
-            }
-        }
+    //console.log(decodeBits());
+    //console.log('data', dataArray);
+    //let index = dataArray.indexOf(Math.max(...dataArray));
+    //console.log(freqs[index]);
 
-        // for (let i = 0; i < dlen; i++) {
-        //     p.rect(i * binWidth, 300 - dataArray[i] * 2, binWidth, dataArray[i] * 2);
-        // }
+    for (let i in dataArray) {
+      if (dataArray[i] > INTENSITY_THRESHOLD) {
+        // console.log(i);
+      }
+    }
 
-        p.strokeWeight(6);
-        let c1 = p.color(86, 151, 95);
-        let c2 = p.color(148, 69, 69);
-        // let c1 = p.color(0, 0, 0);
-        // let c2 = p.color(255, 255, 255);
+    // for (let i = 0; i < dlen; i++) {
+    //     p.rect(i * binWidth, 300 - dataArray[i] * 2, binWidth, dataArray[i] * 2);
+    // }
 
-        // for (let i = 2; i < dlen; i = i + 9) {
-        //     let h = i < dlen / 2 ? dataArray[i] * 0.75 : dataArray[dlen - i] * 0.75;
-        //     let theta = -Math.PI / 2 + (2 * i * Math.PI) / dlen;
-        //     p.stroke(p.lerpColor(c1, c2, Math.sin(2 * Math.PI * (t + i / dlen))));
-        //     //     p.stroke(`rgba(86, 151, 95,${1 - i / dlen})`);
-        //     // + Math.exp(-Math.sin(2 * Math.PI * t) ^ 2) * r * 0.1,
-        //     //     let off = Math.cos((6 * Math.PI * i * t) / dlen) * r * 0.1;
-        //     let off = 0;
-        //     p.line(
-        //         xc + r * Math.cos(theta) + off,
-        //         yc + r * Math.sin(theta) + off,
-        //         xc + (r + h / 2) * Math.cos(theta) + off,
-        //         yc + (r + h / 2) * Math.sin(theta) + off
-        //     );
-        // }
+    p.strokeWeight(6);
+    let c1 = p.color(86, 151, 95);
+    let c2 = p.color(148, 69, 69);
+    // let c1 = p.color(0, 0, 0);
+    // let c2 = p.color(255, 255, 255);
 
-        p.stroke("rgb(0,0,0)");
-        p.beginShape();
-        // p.noFill();
-        p.fill(p.lerpColor(c1, c2, Math.sin(Math.PI * t) ** 2));
-        let first = {x: 0, y: 0}
-        for (let i = 0; i < dlen + 10; i = i + 8) {
-            let h =
-                i < dlen / 2
-                    ? dataArray[i] * 0.75
-                    : dataArray[dlen + 10 - i] * 0.75;
-            let theta = -Math.PI / 2 + (2 * i * Math.PI) / dlen;
-            //     p.stroke(`rgba(86, 151, 95,${1 - i / dlen})`);
-            // + Math.exp(-Math.sin(2 * Math.PI * t) ^ 2) * r * 0.1,
-            //     let off = Math.cos((6 * Math.PI * i * t) / dlen) * r * 0.1;
-            if (i === 0) first = {x: xc + (r + h * 0.75) * Math.cos(theta), y: yc + (r + h * 0.75) * Math.sin(theta)} 
-            p.curveVertex(
-                xc + (r + h * 0.75) * Math.cos(theta),
-                yc + (r + h * 0.75) * Math.sin(theta)
-            );
-        }
-        
-        p.curveVertex(first.x, first.y);
-        p.endShape(p.CLOSE);
+    // for (let i = 2; i < dlen; i = i + 9) {
+    //     let h = i < dlen / 2 ? dataArray[i] * 0.75 : dataArray[dlen - i] * 0.75;
+    //     let theta = -Math.PI / 2 + (2 * i * Math.PI) / dlen;
+    //     p.stroke(p.lerpColor(c1, c2, Math.sin(2 * Math.PI * (t + i / dlen))));
+    //     //     p.stroke(`rgba(86, 151, 95,${1 - i / dlen})`);
+    //     // + Math.exp(-Math.sin(2 * Math.PI * t) ^ 2) * r * 0.1,
+    //     //     let off = Math.cos((6 * Math.PI * i * t) / dlen) * r * 0.1;
+    //     let off = 0;
+    //     p.line(
+    //         xc + r * Math.cos(theta) + off,
+    //         yc + r * Math.sin(theta) + off,
+    //         xc + (r + h / 2) * Math.cos(theta) + off,
+    //         yc + (r + h / 2) * Math.sin(theta) + off
+    //     );
+    // }
 
-        p.fill("rgb(0, 0, 0)");
-        // p.stroke("rgba(0,0,0)");
-        p.circle(xc, yc, 2 * r);
+    p.stroke("rgb(0,0,0)");
+    p.beginShape();
+    // p.noFill();
+    p.fill(p.lerpColor(c1, c2, Math.sin(Math.PI * t) ** 2));
+    for (let i = 0; i <= dlen + 9; i = i + 8) {
+      let h =
+        i < dlen / 2 ? dataArray[i] * 0.75 : dataArray[dlen + 8 - i] * 0.75;
+      let theta = -Math.PI / 2 + (2 * i * Math.PI) / dlen;
+      //     p.stroke(`rgba(86, 151, 95,${1 - i / dlen})`);
+      // + Math.exp(-Math.sin(2 * Math.PI * t) ^ 2) * r * 0.1,
+      //     let off = Math.cos((6 * Math.PI * i * t) / dlen) * r * 0.1;
+      p.curveVertex(
+        xc + (r + h * 0.75) * Math.cos(theta),
+        yc + (r + h * 0.75) * Math.sin(theta)
+      );
+    }
+    p.endShape();
 
-        //    if (timer % timerInterval === 0) {
-        //         let signal = decodeBits()
-        //         console.log(signal)
-        //         // uncomment to console log most prominent frequency
-        //         ///let index = dataArray.indexOf(Math.max(...dataArray))
-        //         // console.log(freqs[index])
-        //     }
-        binWidth = screenWidth / dataArray.length;
-        for (let i = 0; i < dataArray.length; i++) {
-            p.rect(
-                i * binWidth,
-                600 - dataArray[i] * 2,
-                binWidth,
-                dataArray[i] * 2
-            );
-        }
-    };
+    p.fill("rgb(0, 0, 0)");
+    // p.stroke("rgba(0,0,0)");
+    p.circle(xc, yc, 2 * r);
+
+    //    if (timer % timerInterval === 0) {
+    //         let signal = decodeBits()
+    //         console.log(signal)
+    //         // uncomment to console log most prominent frequency
+    //         ///let index = dataArray.indexOf(Math.max(...dataArray))
+    //         // console.log(freqs[index])
+    //     }
+    binWidth = screenWidth / dataArray.length;
+    // for (let i = 0; i < dataArray.length; i++) {
+    //   p.rect(i * binWidth, 600 - dataArray[i] * 2, binWidth, dataArray[i] * 2);
+    // }
+  };
 };
 
 const onStream = (stream) => {
-    var inputPoint = audioContext.createGain();
-    // Create an AudioNode from the stream
-    var realAudioInput = audioContext.createMediaStreamSource(stream);
-    var audioInput = realAudioInput;
-    audioInput.connect(inputPoint);
-    inputPoint.connect(analyserNode);
+  var inputPoint = audioContext.createGain();
+  // Create an AudioNode from the stream
+  var realAudioInput = audioContext.createMediaStreamSource(stream);
+  var audioInput = realAudioInput;
+  audioInput.connect(inputPoint);
+  inputPoint.connect(analyserNode);
 
-    setInterval(recalculateFourier, Math.round(UNIT_TIME / UNIT_LENGTH));
+  setInterval(recalculateFourier, Math.round(UNIT_TIME / UNIT_LENGTH));
 
-    // start drawing
-    started = true;
+  // start drawing
+  started = true;
 };
 
 export const initAudio = () => {
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    analyserNode = audioContext.createAnalyser();
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  analyserNode = audioContext.createAnalyser();
 
-    analyserNode.fftSize = 4096 / 2;
-    bufferLength = analyserNode.frequencyBinCount;
-    dataArray = new Uint8Array(bufferLength);
+  analyserNode.fftSize = 4096 / 2;
+  bufferLength = analyserNode.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
 
-    sampleRate = audioContext.sampleRate;
-    bandWidth = sampleRate / bufferLength;
+  sampleRate = audioContext.sampleRate;
+  bandWidth = sampleRate / bufferLength;
 
-    for (let i = 0; i < bufferLength; i++) {
-        freqs.push((i * bandWidth) / 2);
-    }
+  for (let i = 0; i < bufferLength; i++) {
+    freqs.push((i * bandWidth) / 2);
+  }
 
-    if (!navigator.getUserMedia)
-        navigator.getUserMedia =
-            navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    if (!navigator.cancelAnimationFrame)
-        navigator.cancelAnimationFrame =
-            navigator.webkitCancelAnimationFrame ||
-            navigator.mozCancelAnimationFrame;
-    if (!navigator.requestAnimationFrame)
-        navigator.requestAnimationFrame =
-            navigator.webkitRequestAnimationFrame ||
-            navigator.mozRequestAnimationFrame;
+  if (!navigator.getUserMedia)
+    navigator.getUserMedia =
+      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  if (!navigator.cancelAnimationFrame)
+    navigator.cancelAnimationFrame =
+      navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+  if (!navigator.requestAnimationFrame)
+    navigator.requestAnimationFrame =
+      navigator.webkitRequestAnimationFrame ||
+      navigator.mozRequestAnimationFrame;
 
-    navigator.mediaDevices
-        .getUserMedia({ audio: true, video: false })
-        .then((stream) => {
-            onStream(stream);
-        })
-        .catch((err) => {
-            alert(err);
-            console.log(err);
-        });
-    //navigator.getUserMedia(
-    //{
-    //audio: {
-    //mandatory: {
-    //googEchoCancellation: "false",
-    //googAutoGainControl: "false",
-    //googNoiseSuppression: "false",
-    //googHighpassFilter: "false",
-    //},
-    //optional: [],
-    //},
-    //optional: [],
-    //},
-    //
-    //onStream,
-    //function (e) {
-    //alert("Couldn't connect to an audio device");
-    //console.log(e);
-    //}
-    //);
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then((stream) => {
+      onStream(stream);
+    })
+    .catch((err) => {
+      alert(err);
+      console.log(err);
+    });
+  //navigator.getUserMedia(
+  //{
+  //audio: {
+  //mandatory: {
+  //googEchoCancellation: "false",
+  //googAutoGainControl: "false",
+  //googNoiseSuppression: "false",
+  //googHighpassFilter: "false",
+  //},
+  //optional: [],
+  //},
+  //optional: [],
+  //},
+  //
+  //onStream,
+  //function (e) {
+  //alert("Couldn't connect to an audio device");
+  //console.log(e);
+  //}
+  //);
 };
 
 const restart_state_machine = () => {
-    sample_buffer = [];
-    unit_buffer = [];
-    sample_time_buffer = [];
-    state = 1;
-    header_pos = 1;
+  sample_buffer = [];
+  unit_buffer = [];
+  sample_time_buffer = [];
+  state = 1;
+  header_pos = 1;
 };
 
 const get_current_second = () => {
-    return Math.floor(new Date()/1000);
-}
+  return Math.floor(new Date() / 1000);
+};
 
 //States:
 //1 - no signal detected
@@ -291,96 +283,94 @@ const get_current_second = () => {
 let startSecond = new Date();
 let frame_counter = 1;
 
-const decode_message = n => {
-    if (new Date() - startSecond >= frame_counter*1000) {
-        let value = get_sample_buffer_value(sample_buffer);
-        console.log(state, value, sample_buffer)
+const decode_message = (n) => {
+  if (new Date() - startSecond >= frame_counter * 1000) {
+    let value = get_sample_buffer_value(sample_buffer);
+    console.log(state, value, sample_buffer);
 
-        if (state === 1) {
-            let i = 0;
-            let num_seen = 0;
-            while (num_seen < 3 && i < sample_buffer.length) {
-                if (sample_buffer[i] === 136) {
-                    num_seen++;
-                } else {
-                    num_seen = 0;
-                }
-                i++;
-            }
-            i -= 3;
-            if (num_seen === 3) {
-                console.log("Detected header starting in index " + i);
-                state = 4;
-                //pastSecond.setMilliseconds(sample_time_buffer[i].getMilliseconds());
-                startSecond = new Date();
-                frame_counter = 0;
-            }
-        } else if (state === 4) {
-            //Just discard it as it was past one
-            state = 2;
-        } else if (state === 2) {
-            length = value;
-            state = 3;
-        } else if (state === 3) {
-            unit_buffer.push(value);
-            if (unit_buffer.length === length) {
-                console.log("MESSAGE RECEIVED:");
-                console.log("Length:");
-                console.log(length);
-                console.log("Payload:");
-                console.log(unit_buffer);
-                let message = ""
-                for (code of unit_buffer) {
-                    message = message + String.fromCharCode(code);
-                }
-                console.log(message)
-                passMsg(message);
-                state = 1;
-                sample_buffer = [];
-                unit_buffer = [];
-            }
+    if (state === 1) {
+      let i = 0;
+      let num_seen = 0;
+      while (num_seen < 3 && i < sample_buffer.length) {
+        if (sample_buffer[i] === 136) {
+          num_seen++;
+        } else {
+          num_seen = 0;
         }
+        i++;
+      }
+      i -= 3;
+      if (num_seen === 3) {
+        console.log("Detected header starting in index " + i);
+        state = 4;
+        //pastSecond.setMilliseconds(sample_time_buffer[i].getMilliseconds());
+        startSecond = new Date();
+        frame_counter = 0;
+      }
+    } else if (state === 4) {
+      //Just discard it as it was past one
+      state = 2;
+    } else if (state === 2) {
+      length = value;
+      state = 3;
+    } else if (state === 3) {
+      unit_buffer.push(value);
+      if (unit_buffer.length === length) {
+        console.log("MESSAGE RECEIVED:");
+        console.log("Length:");
+        console.log(length);
+        console.log("Payload:");
+        console.log(unit_buffer);
+        let message = "";
+        for (code of unit_buffer) {
+          message = message + String.fromCharCode(code);
+        }
+        console.log(message);
+        passMsg(message);
+        state = 1;
         sample_buffer = [];
-        sample_time_buffer = [];
-        frame_counter++;
+        unit_buffer = [];
+      }
     }
+    sample_buffer = [];
+    sample_time_buffer = [];
+    frame_counter++;
+  }
 
-
-    let milli = new Date() % 1000;
-    if (state===1 || (300 < milli && milli < 900)) {
-        sample_buffer.push(n);
-        sample_time_buffer.push(new Date());
-    }
-}
-
+  let milli = new Date() % 1000;
+  if (state === 1 || (300 < milli && milli < 900)) {
+    sample_buffer.push(n);
+    sample_time_buffer.push(new Date());
+  }
+};
 
 /**
  * Returns the most common number in the buffer if it is above the THRESHOLD.
  * Returns -1 if nothing above THRESHOLD
  */
 const get_sample_buffer_value = (buff) => {
-    seen_values = {}
-    current_max = null;
-    for (value of buff) {
-        if (value in seen_values) {
-            seen_values[value] += 1;
-            if (current_max == null) {
-                current_max = value;
-            } else {
-                if (seen_values[value] > seen_values[current_max]) {
-                    current_max = value;
-                }
-            }
-        } else {
-            seen_values[value] = 1
+  seen_values = {};
+  current_max = null;
+  for (value of buff) {
+    if (value in seen_values) {
+      seen_values[value] += 1;
+      if (current_max == null) {
+        current_max = value;
+      } else {
+        if (seen_values[value] > seen_values[current_max]) {
+          current_max = value;
         }
-    }
-
-    if (seen_values[current_max] >= HEADER_THRESHOLD*buff.length) {
-        return current_max;
+      }
     } else {
-        return -1;
+      seen_values[value] = 1;
     }
-}
+  }
+
+  if (seen_values[current_max] >= HEADER_THRESHOLD * buff.length) {
+    return current_max;
+  } else {
+    return -1;
+  }
+};
 
 export default sketch;
